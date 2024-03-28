1e9d0fee64b8c340c1dd7685ed93f49fc6736ac1
José Domínguez <jndominguez@uc.cl>
2024-03-28 15:52:50 -0300

    formated nodes and edges

--- debugger_agent/src/graph_construction/db_manager.py
+++ debugger_agent/src/graph_construction/db_manager.py
@@ -5,6 +5,7 @@ from typing import List, Any
 class JSONManager:
     def __init__(self, default_path: str = "graph.json"):
         self.default_path = default_path
+        self.node_dict = {}
 
     def save_graph(self, nodes: List[Any], edges: List[Any], path: str = None):
         if path is None:
@@ -16,19 +17,20 @@ class JSONManager:
 
     def format_node(self, node):
         formatted_node = {
-            "id": node["attributes"]["node_id"],
+            "id": str(uuid.uuid4()),
             "properties": {
                 **node["attributes"],
                 "label": node["type"]
             }
         }
+        self.node_dict[str(formatted_node["properties"]["node_id"])] = formatted_node["id"]
         return formatted_node
 
     def format_edge(self, edge):
         formatted_edge = {
             "id": str(uuid.uuid4()),
-            "start": edge["sourceId"],
-            "end": edge["targetId"],
+            "start": self.node_dict[str(edge["sourceId"])],
+            "end": self.node_dict[str(edge["targetId"])],
             "properties": {
                 "label": edge["type"]
             }
