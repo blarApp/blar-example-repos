08b0ff5d3df0d752166aa9485d9e13f362c89af1
José Domínguez <jndominguez@uc.cl>
2024-03-28 15:28:01 -0300

    format node for yFiles

--- debugger_agent/graph_construction/db_manager.py
+++ debugger_agent/graph_construction/db_manager.py
@@ -1,7 +1,8 @@
-from typing import Any, List
 import json
+import uuid
+from typing import List, Any
 
-class JSONManager():
+class JSONManager:
     def __init__(self, default_path: str = "graph.json"):
         self.default_path = default_path
 
@@ -9,6 +10,27 @@ class JSONManager():
         if path is None:
             path = self.default_path
         with open(path, "w") as f:
-            json.dump({"nodes": nodes, "edges": edges}, f)
+            formatted_nodes = list(map(self.format_node, nodes))
+            formatted_edges = list(map(self.format_edge, edges))
+            json.dump({"nodes": formatted_nodes, "edges": formatted_edges}, f, indent=4)
 
-    
+    def format_node(self, node):
+        formatted_node = {
+            "id": node["attributes"]["node_id"],
+            "properties": {
+                **node["attributes"],
+                "label": node["type"]
+            }
+        }
+        return formatted_node
+
+    def format_edge(self, edge):
+        formatted_edge = {
+            "id": str(uuid.uuid4()),
+            "start": edge["sourceId"],
+            "end": edge["targetId"],
+            "properties": {
+                "label": edge["type"]
+            }
+        }
+        return formatted_edge
