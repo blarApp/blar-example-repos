a723a0b206085ac461af2562d607cb7aac6f3dee
berrazuriz1 <berrazuriz1@uc.cl>
2024-04-01 14:03:24 -0300

    add: unit test example

---
+++ unit_test_agent/average_calculator.py
@@ -0,0 +1,16 @@
+# average_calculator.py
+
+from math_operations import add, divide
+
+
+def calculate_average(numbers):
+    """Calculate the average of a list of numbers."""
+    if not numbers:
+        raise ValueError("The list of numbers cannot be empty.")
+
+    sum_of_numbers = 0
+    for number in numbers:
+        sum_of_numbers = add(sum_of_numbers, number)
+
+    average = divide(sum_of_numbers, len(numbers))
+    return average
