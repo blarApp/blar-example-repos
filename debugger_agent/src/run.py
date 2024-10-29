95aee8e8fe6e22eee6891ab35d314285fc50944a
elopezn20 <elopezn20@gmail.com>
2024-10-29 11:02:46 -0300

    Update run.py

--- debugger_agent/src/run.py
+++ debugger_agent/src/run.py
@@ -6,7 +6,7 @@ graph_constructor = GraphConstructor(graph_manager)
 graph_constructor.build_graph("src", "python")
 
 print("running test 29/10")
-for i in range(1, 7):
+for i in range(1, 12):
     print(i)
-    print("super")
+    print("great")
 
