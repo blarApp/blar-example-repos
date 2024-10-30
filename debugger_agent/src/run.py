7eef265526d020b2ea963f206692464c92a5edf6
elopezn20 <elopezn20@gmail.com>
2024-10-30 11:23:01 -0300

    Update run.py

--- debugger_agent/src/run.py
+++ debugger_agent/src/run.py
@@ -5,7 +5,7 @@ graph_manager = JSONManager()
 graph_constructor = GraphConstructor(graph_manager)
 graph_constructor.build_graph("src", "python")
 
-print("finalizing test 30/10")
+print("hello world")
 
 i = 1
 i += 4
