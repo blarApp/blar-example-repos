ad1f544644596246e1c6287011559a69b9538885
elopezn20 <elopezn20@gmail.com>
2024-11-07 18:14:53 -0300

    Update is_prime_number.py

--- is_prime_number.py
+++ is_prime_number.py
@@ -2,6 +2,7 @@ def is_prime(number):
     if number < 2:
         return False
     for i in range(2, number):
+        
         if number % i == 0:
             return True
 
@@ -13,4 +14,10 @@ def total_sum(n):
     total = 0
     for i in range(1, n + 1):
         total += i
+    return total
+
+def total_sum2(n):
+    total = 0
+    for i in range(1, n + 1):
+        total -= i
     return total
