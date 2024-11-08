46a8bf3c0de50694514729f783810726f8549abe
elopezn20 <elopezn20@gmail.com>
2024-11-08 11:17:34 -0300

    Create house.py

---
+++ house.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self, address, num_rooms, price):
+        self.address = address
+        self.num_rooms = num_rooms
+        self.price = price
+
+    def display_info(self):
+        print(f"Address: {self.address}")
+        print(f"Number of rooms: {self.num_rooms}")
+        print(f"Price: ${self.price}")
