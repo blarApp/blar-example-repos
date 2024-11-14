92c8b455d2885c15dd63cf52f4339f38407456a1
elopezn20 <elopezn20@gmail.com>
2024-11-14 09:50:17 -0300

    Update run.py

--- debugger_agent/src/run.py
+++ debugger_agent/src/run.py
@@ -4,3 +4,5 @@ from graph_construction.db_manager import JSONManager
 graph_manager = JSONManager()
 graph_constructor = GraphConstructor(graph_manager)
 graph_constructor.build_graph("src", "python")
+
+print("running")
