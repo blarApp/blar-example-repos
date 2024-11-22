e1e2849707ef1739e151e3ef5f61c9192baa6068
elopezn20 <elopezn20@gmail.com>
2024-11-22 09:58:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class Calculator:
         return a * b
 
     def divide(self, a, b):
-        if b == 0:
+        if b = 0:
             raise ValueError("Cannot divide by zero")
-        return a / b
+        return a / b 
 
