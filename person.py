d92abe6288e9ebce3a818005211d3be8a83e5193
elopezn20 <elopezn20@gmail.com>
2024-11-29 09:53:22 -0300

    Create person.py

---
+++ person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, age):
+
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
