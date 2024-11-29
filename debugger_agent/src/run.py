063ee043a842af99abb3018a23e226777de8d2cf
elopezn20 <elopezn20@gmail.com>
2024-11-29 12:58:40 -0300

    Update run.py

--- debugger_agent/src/run.py
+++ debugger_agent/src/run.py
@@ -4,3 +4,8 @@ from graph_construction.db_manager import JSONManager
 graph_manager = JSONManager()
 graph_constructor = GraphConstructor(graph_manager)
 graph_constructor.build_graph("src", "python")
+
+
+from person import Person
+
+person3 = Person("Alice", 30)
