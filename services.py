ff25785890424008ee1ca70ca1806d9db484a408
elopezn20 <elopezn20@gmail.com>
2024-12-17 11:45:46 -0300

    remove append

--- services.py
+++ services.py
@@ -11,7 +11,7 @@ class TaskService:
     def add_task(self, title, description, due_date, status="Pending"):
         task_id = len(self.tasks) + 1
         task = Task(task_id, title, description, due_date, status)
-        self.tasks.append(task)
+        
 
     def get_due_tasks(self):
         return [task for task in self.tasks if task.is_due()]
