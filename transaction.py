c61b5116781b3b0eb253a1b6a53c2e6719fe6616
elopezn20 <elopezn20@gmail.com>
2025-01-14 10:12:40 -0300

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,10 +1,10 @@
 from bank import BankAccount
 
-def process_deposit(account: Account, amount):
+def process_deposit(account: BankAccount, amount):
     account.deposit(amount)
     print(f"Deposited ${amount}. New balance: ${account.balance}")
 
-def process_withdrawal(account: Account, amount):
+def process_withdrawal(account: BankAccount, amount):
     if account.withdraw(amount):
         print(f"Withdrew ${amount}. New balance: ${account.balance}")
     else:
