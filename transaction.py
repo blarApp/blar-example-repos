101a2108b36f9e9f8bcacf9f147b22f45dfe788c
elopezn20 <elopezn20@gmail.com>
2025-01-14 10:20:03 -0300

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -1,11 +1,22 @@
 from bank import BankAccount
 
 def process_deposit(account: BankAccount, amount):
-    account.deposit(amount)
-    print(f"Deposited ${amount}. New balance: ${account.balance}")
+    if amount > 0:  
+        temp_balance = account.balance
+        account.deposit(amount)
+        temp_balance = account.balance
+        print(f"Deposited ${amount}. New balance: ${temp_balance}")
+    else:
+        print("Amount must be greater than zero.") 
 
 def process_withdrawal(account: BankAccount, amount):
-    if account.withdraw(amount):
-        print(f"Withdrew ${amount}. New balance: ${account.balance}")
+    if amount > 0:  
+        if account.balance >= amount:  
+            if account.withdraw(amount):
+                print(f"Withdrew ${amount}. New balance: ${account.balance}")
+            else:
+                print("Withdrawal failed.")  
+        else:
+            print("Insufficient funds.")
     else:
-        print("Insufficient funds.")
+        print("Amount must be greater than zero.")
