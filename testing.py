d44111301693ae330091d8b8be645e16b9dd3755
elopezn20 <elopezn20@gmail.com>
2025-01-20 15:16:35 -0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,6 @@ from book import Book
 
 new_book = Book("bob", "testing", 100)
 new_book.summary()
-new_book.info()
 
 
 old_book = Book("alice", "testing", 200)
@@ -10,3 +9,5 @@ old_book.summary()
 
 book = Book("alice", "testing", 200)
 book.summary()
+book.call_book()
+print(book.is_long_book())
