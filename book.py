fe010c0bc859b2a14b99dbb4c0ca47b9f1e48a18
elopezn20 <elopezn20@gmail.com>
2025-01-31 10:31:49 -0300

    Update book.py

--- book.py
+++ book.py
@@ -4,6 +4,9 @@ class Book:
         self.author = author
         self.pages = pages
 
+    def summary(self, number_of_pages):
+        print(f"'the book {self.title}' by {self.author}, {self.pages} pages long and i read {number_of_pages}")
+        
     def is_long_book(self):
         return self.pages > 400
 
